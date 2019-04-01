57bf592a0831643e4d39eabb1aa388b116a991b0
2019-DEV-131 <48527986+2019-DEV-131@users.noreply.github.com>
2019-04-01 19:57:02 +0530

    Added xcode project files
    Project setup completed

---
+++ BerlinClock-DEV131Tests/BerlinClock_DEV131Tests.swift
@@ -0,0 +1,17 @@
+import XCTest
+@testable import BerlinClock_DEV131
+
+class BerlinClock_DEV131Tests: XCTestCase {
+    
+    override func setUp() {
+        
+    }
+
+    override func tearDown() {
+        
+    }
+    
+    func test_Berlin_Clock() {
+        
+    }
+}
