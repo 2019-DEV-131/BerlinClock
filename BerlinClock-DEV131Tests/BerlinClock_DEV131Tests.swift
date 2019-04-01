e4fd2add54dbc0547c04fa71a95e0cd5ec9ea442
2019-DEV-131 <48527986+2019-DEV-131@users.noreply.github.com>
2019-04-01 20:51:55 +0530

    Update BerlinClock_DEV131Tests.swift
    - Added tests for five hour lamps colors
    - Added tests for different hours values for five hour lamps

--- BerlinClock-DEV131Tests/BerlinClock_DEV131Tests.swift
+++ BerlinClock-DEV131Tests/BerlinClock_DEV131Tests.swift
@@ -37,6 +37,27 @@ extension BerlinClock_DEV131Tests {
 // Testing five hour lamp status
 extension BerlinClock_DEV131Tests {
     
+    func test_FiveHour_LampState_Counts_00_Hours() {
+        sut = BerlinClock(0, 51, 58)
+        
+        let lamps = sut.getOutput()
+        XCTAssertEqual(lamps.fiveHourLamps.count, 0)
+    }
+    
+    func test_FiveHour_LampState_Counts_01_Hours() {
+        sut = BerlinClock(1, 51, 58)
+        
+        let lamps = sut.getOutput()
+        XCTAssertEqual(lamps.fiveHourLamps.count, 0)
+    }
+    
+    func test_FiveHour_LampState_Counts_05_Hours() {
+        sut = BerlinClock(5, 51, 58)
+        
+        let lamps = sut.getOutput()
+        XCTAssertEqual(lamps.fiveHourLamps.count, 1)
+    }
+    
     func test_FiveHour_LampState_Counts_16_Hours() {
         sut = BerlinClock(16, 51, 58)
         
@@ -44,6 +65,21 @@ extension BerlinClock_DEV131Tests {
         XCTAssertEqual(lamps.fiveHourLamps.count, 3)
     }
     
+    func test_FiveHour_LampState_Counts_23_Hours() {
+        sut = BerlinClock(23, 51, 58)
+        
+        let lamps = sut.getOutput()
+        XCTAssertEqual(lamps.fiveHourLamps.count, 4)
+    }
+    
+    
+    func test_FiveHour_LampState_Colors_05_Hours() {
+        sut = BerlinClock(5, 51, 58)
+        
+        let expected: [BerlinClock.LampColor] = [ .red ]
+        let lamps = sut.getOutput()
+        XCTAssertEqual(lamps.fiveHourLamps, expected)
+    }
 }
 
 // Testing single hour lamp status
